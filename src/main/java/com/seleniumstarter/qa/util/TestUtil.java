b8aeddf8586f6e8ccf3ee6678e69844d89d625b3
timothymwine <timzytim@gmail.com>
2022-08-12 22:42:23 +0300

    main
    added getRowCount

--- src/main/java/com/seleniumstarter/qa/util/TestUtil.java
+++ src/main/java/com/seleniumstarter/qa/util/TestUtil.java
@@ -1,7 +1,25 @@
 package com.seleniumstarter.qa.util;
 
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 public class TestUtil {
 
+    public static XSSFWorkbook workbook;
     public static long PAGE_LOAD_TIMEOUT = 30;
     public static long IMPLICIT_WAIT = 20;
+
+    public static void getRowCount() throws IOException {
+
+        String dataPath =
+                "/Users/mwine/workspace/seleniumstarterproject/src/main/resources/com/"
+                        + "seleniumstarter/qa/testdata/testdata.xlsx";
+
+        workbook = new XSSFWorkbook( dataPath );
+        XSSFSheet sheet = workbook.getSheet( "Sheet1" );
+        int rowCount = sheet.getPhysicalNumberOfRows();
+        System.out.println( "number of rows is :" + rowCount );
+    }
 }
