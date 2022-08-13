296bda8627d66a1fb70133507d23356f5bb50bc6
timothymwine <timzytim@gmail.com>
2022-08-13 06:10:28 +0300

    main
    changed testdata.xlsx location
    added getTestData to TestUtil class

--- src/main/java/com/seleniumstarter/qa/util/TestUtil.java
+++ src/main/java/com/seleniumstarter/qa/util/TestUtil.java
@@ -1,9 +1,12 @@
 package com.seleniumstarter.qa.util;
 
 import java.io.IOException;
+import java.io.InputStream;
 
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 
 /**
  * @author Timothy Mwine
@@ -11,19 +14,37 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
  */
 public class TestUtil {
 
-    public static XSSFWorkbook workbook;
+    public static Workbook book;
+    public static Sheet sheet;
+
     public static long PAGE_LOAD_TIMEOUT = 30;
     public static long IMPLICIT_WAIT = 20;
 
-    public static void getRowCount() throws IOException {
+    public static Object[][] getTestData( String sheetName ) {
 
-        String dataPath =
-                "/Users/mwine/workspace/seleniumstarterproject/src/main/resources/com/"
-                        + "seleniumstarter/qa/testdata/testdata.xlsx";
+        InputStream file = null;
+        file = TestUtil.class.getResourceAsStream( "testdata.xlsx" );
+        try {
 
-        workbook = new XSSFWorkbook( dataPath );
-        XSSFSheet sheet = workbook.getSheet( "Sheet1" );
-        int rowCount = sheet.getPhysicalNumberOfRows();
-        System.out.println( "number of rows is :" + rowCount );
+            book = WorkbookFactory.create( file );
+        }
+        catch ( EncryptedDocumentException e ) {
+
+            throw new RuntimeException( "testdata.xlsx is encrypted", e );
+        }
+        catch ( IOException e ) {
+
+            throw new RuntimeException( "testdata.xlsx is missing", e );
+        }
+        sheet = book.getSheet( sheetName );
+        Object[][] data = new Object[sheet.getLastRowNum()][sheet.getRow( 0 ).getLastCellNum()];
+        for ( int i = 0; i < sheet.getLastRowNum(); i++ ) {
+
+            for ( int k = 0; k < sheet.getRow( 0 ).getLastCellNum(); k++ ) {
+
+                data[i][k] = sheet.getRow( i + 1 ).getCell( k ).toString();
+            }
+        }
+        return data;
     }
 }
